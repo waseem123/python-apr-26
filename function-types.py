6a7e343614ef2a1f74a1451685866e6bb2e26779
waseem123 <waseem_6957@yahoo.com>
2026-05-12 20:19:30 +0530

    function types

---
+++ function-types.py
@@ -0,0 +1,26 @@
+# 1. Default Arg functions
+# 2. Keyword Arg functions
+# 3. Arbitrary Arg functions
+# 4. Keyword Arbitrary Arg functions
+
+# 1. Default Arg Functions
+def showData(name="xyz",city="pqr"):
+    print(f'My name is {name}. I live in {city}')
+    
+showData()
+showData("waseem","solapur")
+# for i in range(1,100000):
+#     showData("Solapur")
+
+# 2. Keyword Arg functions
+def showName(fname,lname,city):
+    print(f'My full name is {fname} {lname}. I live in {city}')
+    
+showName('Waseem','Attar','Solapur')
+showName(city='Pune',lname='Attar',fname='Waseem')
+
+# 3. Arbitrary Arg functions
+def showValues(*data):
+    print(f"My name is {data[0]} {data[1]}")
+    
+showValues('Waseem','Attar','Solapur')
