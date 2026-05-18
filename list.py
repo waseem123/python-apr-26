bed60f077bfb1338c91ffc282be53cb1fa2894a2
waseem123 <waseem_6957@yahoo.com>
2026-05-18 20:21:27 +0530

    List Tutorial

---
+++ list.py
@@ -0,0 +1,18 @@
+# Lists are mutable, sequential and reapeated data collection.
+
+x  = [100,200,300,400,500]
+print(x)
+print(type(x))
+print(len(x))
+print('--------------------')
+print(x[0])
+print(x[1])
+print(x[2])
+print(x[3])
+print(x[4])
+# print(x[5])
+
+# List creation using List Constructor
+cities = list(('Mumbai','Pune','Solapur','Hyderabad','Chennai','Delhi','Kolkata'))
+print(cities)
+print(type(cities))
